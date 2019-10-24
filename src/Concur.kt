1bbc97105832b3792f8950b7ae00de99afe1601e
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-10-24 16:24:09 +0200

    Cleanup.

--- src/Concur.kt
+++ src/Concur.kt
@@ -8,39 +8,29 @@ import lambdada.parsec.parser.*
  */
 
 // Function to build parsers with.
-// Should call recursive function.
-// was: processConcur(p1, p2, p3, p4, it)
 fun <I, A> concur(p1: Parser<I, A>, p2: Parser<I, A>, p3: Parser<I, A>, p4: Parser<I, A>):
-        Parser<I, List<A>> = { processConcur2(listOf(p1, p2, p3, p4), listOf(), it) }
+        Parser<I, List<A>> = { processConcurRecursive(listOf(p1, p2, p3, p4), listOf(), it) }
 
-// TODO: incomplete implementation
-fun <I, A> processConcur(p1: Parser<I, A>, p2: Parser<I, A>, p3: Parser<I, A>, p4: Parser<I, A>, reader: Reader<I>): Response<I, List<A>> {
-    val pa = p1.then(p2)(reader) as Response.Accept
-    val pa2 = (p3.then(p4)(pa.input) as Response.Accept).value
-    return Response.Accept(listOf(pa.value.first, pa.value.second, pa2.first, pa2.second), reader, true)
-}
-
-// TODO: trying to make it work with a list
-tailrec fun <I, A> processConcur2(parsers: List<Parser<I, A>>, results: List<A>, reader: Reader<I>): Response<I, List<A>> {
-    // we go over all the parsers looking for an Accept.
-    // we map all the parsers to a (parser, response) pair
-    // then we filter to the first accept
+// we go over all the parsers looking for an Accept.
+// we map all the parsers to a (parser, response) pair
+// then we filter to the first accept
+// if no parser was found -> reject
+// if a parser was found and no more parsers are to do -> accept
+// if a parser was found and after filtering list of parsers not empty -> recurse
+// Note: if we no parsers are supplied to this function it will reject.
+// TODO: consumed is always false in case of a reject. That does not have to be the case, one or more parsers could have succeeded!
+tailrec fun <I, A> processConcurRecursive(parsers: List<Parser<I, A>>, results: List<A>, reader: Reader<I>): Response<I, List<A>> {
     val firstParserResponsePair =
         parsers.map { parser -> Pair(parser, parser(reader)) }.find { pair -> pair.second is Response.Accept }
 
-    println(firstParserResponsePair)
-
-    // if no parser was found -> reject
     val response = firstParserResponsePair?.second as Response.Accept<I, A>?
         ?: return Response.Reject(reader.location(), false)
 
-    // if a parser was found and no more parsers are to do -> accept
-    // if a parser was found and after filtering list of parsers not empty -> recurse
     val parsersToDo = parsers.filter { parser -> parser != firstParserResponsePair?.first }
 
     return when (parsersToDo.isEmpty()) {
         true -> Response.Accept(results + response.value, response.input, true)
-        false -> processConcur2(parsersToDo, results + response.value, response.input)
+        false -> processConcurRecursive(parsersToDo, results + response.value, response.input)
     }
 }
 

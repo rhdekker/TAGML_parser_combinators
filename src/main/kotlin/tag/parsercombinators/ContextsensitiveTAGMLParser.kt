3216eab8aeb6cdb23c7b631d93e6133d81363e1d
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-10-30 09:52:06 +0100

    Added expected close tag parser.

--- src/main/kotlin/tag/parsercombinators/ContextsensitiveTAGMLParser.kt
+++ src/main/kotlin/tag/parsercombinators/ContextsensitiveTAGMLParser.kt
@@ -8,13 +8,15 @@ import lambdada.parsec.parser.*
  */
 // We need the basic open and close tags
 // Note that these are in package scope
-val openTagParser: Parser<Char, String> = char('[').thenRight(charIn(CharRange('a', 'z')).rep).thenLeft(char('>')).map {
+val openTagParser: Parser<Char, String> = char('[') thenRight charIn(CharRange('a', 'z')).rep thenLeft char('>') map {
     String(it.toCharArray())
 }
-val closeTagParser: Parser<Char, String> = char('<').thenRight(charIn(CharRange('a', 'z')).rep).thenLeft(char(']')).map {
+val anyCloseTagParser: Parser<Char, String> = char('<').thenRight(charIn(CharRange('a', 'z')).rep).thenLeft(char(']')).map {
     String(it.toCharArray())
 }
-val openAndCloseTagParser: Parser<Char, Pair<String, String>> = openTagParser then closeTagParser
+fun expectedCloseTagParser(expected: String): Parser<Char, String> = char('<') thenRight string(expected) thenLeft char(']')
+
+val openAndCloseTagParser: Parser<Char, Pair<String, String>> = openTagParser then anyCloseTagParser
 
 
 fun main() {

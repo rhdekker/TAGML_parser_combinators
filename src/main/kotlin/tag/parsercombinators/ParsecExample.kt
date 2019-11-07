9a2e6aed5e09ef8f4d1f3e638acf2653477cc4a6
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-11-07 17:28:01 +0100

    Added the after2 function to chain after calls.

--- src/main/kotlin/tag/parsercombinators/ParsecExample.kt
+++ src/main/kotlin/tag/parsercombinators/ParsecExample.kt
@@ -5,9 +5,11 @@ import lambdada.parsec.parser.*
 
 infix fun <I, A> Parser<I, A>.after(p: Parser<I, A>): Parser<I, List<A>> = this flatMap { a -> p map { listOf(a, it) } }
 
+infix fun <I, A> Parser<I, List<A>>.after2(p: Parser<I, A>): Parser<I, List<A>> = this flatMap { a -> p map { a + it } }
+
 fun main() {
 //    val foo: Parser<Char, List<Char>> = not(char(',')).rep
-    val foo: Parser<Char, List<Char>> = char('h') after char('e')
+    val foo: Parser<Char, List<Char>> = char('h') after char('e') after2 char('l')
     val input = Reader.string("hello, parsec!")
     val foobar = foo(input)
     println(foobar)
